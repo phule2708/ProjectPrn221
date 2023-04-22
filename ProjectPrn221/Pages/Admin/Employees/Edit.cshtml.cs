d3396ed62db5c54583b797308733ffed101b5f29
PhuLV6 <105759398+phule2708@users.noreply.github.com>
2023-04-22 10:25:15 +0700

    Employee management

---
+++ ProjectPrn221/Pages/Admin/Employees/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using ProjectPrn221.Models;
+
+namespace ProjectPrn221.Pages.Admin.Employees
+{
+    public class EditModel : PageModel
+    {
+        private readonly PRN221DBContext _context;
+
+        public EditModel(PRN221DBContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Employee Employee { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var employee =  await _context.Employees.FirstOrDefaultAsync(m => m.EmployeeId == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            Employee = employee;
+           ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(Employee.EmployeeId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool EmployeeExists(int id)
+        {
+          return _context.Employees.Any(e => e.EmployeeId == id);
+        }
+    }
+}
---
+++ ProjectPrn221/Pages/Admin/Employees/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProjectPrn221.Models;
+
+namespace ProjectPrn221.Pages.Admin.Employees
+{
+    public class IndexModel : PageModel
+    {
+        private readonly PRN221DBContext _context;
+
+        public IndexModel(PRN221DBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Employee> Employee { get; set; } = default!;
+
+        private int pageSize = 10;
+        public decimal totalPage = 0;
+
+        public async Task OnGetAsync(int pageNum)
+        {
+            if (_context.Employees != null)
+            {
+                pageNum -= 1;
+                if (pageNum <= 0)
+                {
+                    pageNum = 0;
+                }
+                totalPage = Math.Ceiling((decimal)_context.Employees.Count() / pageSize);
+                Employee = await _context.Employees.Skip(pageNum*pageSize).Take(pageSize).Include(e => e.Department).ToListAsync();
+            }
+        }
+    }
+}
